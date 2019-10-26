6f1f876112952fb434090dbd2a5266b1355f3ada
konoway <he.who.can.does@icloud.com>
2019-10-27 03:46:24 +0900

    first commit

---
+++ linebot_lib/define.php
@@ -0,0 +1,4 @@
+<?php
+define('ACCESS_TOKEN','tLEcQdQFaunkBoD4lnY9by0RX9Z4/L4QnevrA2vMKtVaf7u9GBTta5RfhWHzyY+8kJ8qfgf/JfMeBypJ//NGa0dZWyajAVHDBbAb8r+l+ruoQQXyBnDPBkJOa1cO7Vb1l6NLaL4meaq9SqEOrdUTDwdB04t89/1O/w1cDnyilFU=');  //アクセストークン
+define('CHANNEL_SECRET','ded7d3ea16f26accdb3fd514d0129d0c');  //シークレット
+?>
